de692264a5cf0d5119515b2e2c6f9ad27b8d018b
saluma23 <33370484+saluma23@users.noreply.github.com>
2017-11-06 13:21:24 +0530

    Create db.php

---
+++ ui/db.php
@@ -0,0 +1,18 @@
+<?php
+$db=pg_connect('host=localhost:5432
+               dbname=samohdsalman
+               user=samohdsalman
+               password=db-samohdsalman-8710
+               port=5432');
+$query="SELECT * FROM test";
+$result=pg_query($query);
+if(!$result)
+{
+echo"problem";
+exit();
+}
+while($myrow=pg_fetch_assoc($result))
+{
+printf("%s",$myrow['id']);
+}
+?>
