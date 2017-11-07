a2ede5a19961c82992fe51b11901338b4b0a640d
saluma23 <33370484+saluma23@users.noreply.github.com>
2017-11-07 18:26:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/test-db', function (req, res) {
             res.status(500).send(err.toString());
         }
         else{
-           res.send(JSON.stringify(result.rows[1]+result.rows[2]));
+           res.send(JSON.stringify(result.rows[1],result.rows[2]));
         }
         });
 });
